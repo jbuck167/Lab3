a4b8af863c9a214f5e0938f61fd5d937f9492f1c
jbuck167 <jbuck1@udayton.edu>
2017-03-16 21:57:56 -0400

    added main

---
+++ Buck_Lab3/Buck_Lab3/KeyGen.cpp
@@ -0,0 +1,32 @@
+// Author: Josh Buck
+// Course: CPS 471
+
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	// Generate Password 1:
+	std::cout << "Password 1: " << endl;
+
+	// Generate Password 2:
+	std::cout << "Password 2: " << endl;
+
+	// Generate Password 3:
+	std::cout << "Password 3: " << endl;
+
+	// Generate Password 4:
+	std::cout << "Password 4: " << endl;
+
+	// Generate Password 5:
+	std::cout << "Password 5: " << endl;
+
+	std::cout << "Enter any character to close..." << endl;
+	char c;
+	cin >> c;
+
+	return 0;
+}
